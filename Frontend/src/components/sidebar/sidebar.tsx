57a9e8857543915ce469bae81316e2c308430362
Rituraj <agr.rituraj@gmail.com>
2024-12-06 12:40:06 +0530

    adding sidebar

---
+++ Frontend/src/components/sidebar/sidebar.tsx
@@ -0,0 +1,120 @@
+import "./sidebar.css"
+import { useEffect, useRef, useState } from "react";
+import { Link } from 'react-router-dom'
+
+interface SidebarProps {
+    ActiveDashboardLink: number;
+}
+
+function Sidebar(props:SidebarProps) {
+
+    const sidebar = useRef<HTMLDivElement>(null)
+
+    const [activeLink, setActiveLink] = useState(props.ActiveDashboardLink)
+    const [sidebarIsActive, setSidebarIsActive] = useState<boolean>(false)
+
+    const [width, setWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+      const handleResize = () => setWidth(window.innerWidth);
+      window.addEventListener("resize", handleResize);
+      if (sidebar.current) {
+          if (window.innerWidth > 900) {
+                sidebar.current.style.left = "0"
+          } else {
+            sidebar.current.style.left = "-100%"
+          }
+
+      }
+      return () => window.removeEventListener("resize", handleResize);
+    }, [width]);
+
+    const DashboardActivateLink = (index: number) => {
+        setActiveLink(index)
+        if (sidebar.current) {
+            if (window.innerWidth < 900) {
+                  sidebar.current.style.left = "-100%"
+                  handleShowSidebar()
+            }
+        }
+    }
+
+    useEffect(() => {
+        setActiveLink(props.ActiveDashboardLink)
+    })
+
+    const handleShowSidebar = () => {
+        setSidebarIsActive(!sidebarIsActive)
+        if (sidebar.current) {
+            if (!sidebarIsActive) {
+                sidebar.current.style.left = "0"
+            } else {
+                sidebar.current.style.left = "-100%"
+            }
+        }
+    }
+
+    return (
+        <>
+        <div className="navbar">
+            <div className="sidebar_title">AB Bot Panel</div>
+            <div className="navbar-menu" onClick={() => handleShowSidebar()}>
+                <div className={sidebarIsActive ? "lines active" : "lines"}></div>
+            </div>
+        </div>
+        <div ref={sidebar} className="sidebar">
+            <div className="sidebar_title">AB Bot Panel</div>
+            <div className="sidebar_category">
+                <div className="sidebar_subcategory">DASHBOARD</div>
+                <Link to='/' className={activeLink === 0 ? "sidebar_link active" : "sidebar_link"} onClick={() => DashboardActivateLink(0)}>                    
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M520-600v-240h320v240H520ZM120-440v-400h320v400H120Zm400 320v-400h320v400H520Zm-400 0v-240h320v240H120Zm80-400h160v-240H200v240Zm400 320h160v-240H600v240Zm0-480h160v-80H600v80ZM200-200h160v-80H200v80Zm160-320Zm240-160Zm0 240ZM360-280Z"/></svg>
+                <p>Dashboard</p>
+                </Link>
+            </div>
+            <div className="sidebar_category">
+                <div className="sidebar_subcategory">Bots</div>
+                <Link to='/lucky-bot' className={activeLink === 1 ? "sidebar_link active" : "sidebar_link"} onClick={() => DashboardActivateLink(1)}>                    
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M160-360q-50 0-85-35t-35-85q0-50 35-85t85-35v-80q0-33 23.5-56.5T240-760h120q0-50 35-85t85-35q50 0 85 35t35 85h120q33 0 56.5 23.5T800-680v80q50 0 85 35t35 85q0 50-35 85t-85 35v160q0 33-23.5 56.5T720-120H240q-33 0-56.5-23.5T160-200v-160Zm200-80q25 0 42.5-17.5T420-500q0-25-17.5-42.5T360-560q-25 0-42.5 17.5T300-500q0 25 17.5 42.5T360-440Zm240 0q25 0 42.5-17.5T660-500q0-25-17.5-42.5T600-560q-25 0-42.5 17.5T540-500q0 25 17.5 42.5T600-440ZM320-280h320v-80H320v80Zm-80 80h480v-480H240v480Zm240-240Z"/></svg>
+                    <p>Lucky Bot</p> 
+                </Link>
+                <Link to='/fandom-bot' className={activeLink === 2 ? "sidebar_link active" : "sidebar_link"} onClick={() => DashboardActivateLink(2)}>                    
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M200-120q-33 0-56.5-23.5T120-200v-400q0-100 70-170t170-70h240q100 0 170 70t70 170v400q0 33-23.5 56.5T760-120H200Zm0-80h560v-400q0-66-47-113t-113-47H360q-66 0-113 47t-47 113v400Zm160-280q-33 0-56.5-23.5T280-560q0-33 23.5-56.5T360-640q33 0 56.5 23.5T440-560q0 33-23.5 56.5T360-480Zm240 0q-33 0-56.5-23.5T520-560q0-33 23.5-56.5T600-640q33 0 56.5 23.5T680-560q0 33-23.5 56.5T600-480ZM280-200v-80q0-33 23.5-56.5T360-360h240q33 0 56.5 23.5T680-280v80h-80v-80h-80v80h-80v-80h-80v80h-80Zm-80 0h560-560Z"/></svg>
+                <p>Fandom Wiki Bot</p> 
+                </Link>
+                <Link to='/rpg-bot' className={activeLink === 3 ? "sidebar_link active" : "sidebar_link"} onClick={() => DashboardActivateLink(3)}>                    
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M160-120v-200q0-33 23.5-56.5T240-400h480q33 0 56.5 23.5T800-320v200H160Zm200-320q-83 0-141.5-58.5T160-640q0-83 58.5-141.5T360-840h240q83 0 141.5 58.5T800-640q0 83-58.5 141.5T600-440H360ZM240-200h480v-120H240v120Zm120-320h240q50 0 85-35t35-85q0-50-35-85t-85-35H360q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0-80q17 0 28.5-11.5T400-640q0-17-11.5-28.5T360-680q-17 0-28.5 11.5T320-640q0 17 11.5 28.5T360-600Zm240 0q17 0 28.5-11.5T640-640q0-17-11.5-28.5T600-680q-17 0-28.5 11.5T560-640q0 17 11.5 28.5T600-600ZM480-200Zm0-440Z"/></svg>
+                <p>RPG Bot</p> 
+                </Link>
+                <Link to='/personalized-bot' className={activeLink === 4 ? "sidebar_link active" : "sidebar_link"} onClick={() => DashboardActivateLink(4)}>                    
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-200v120h560v-120H568q-17 18-39.5 29T480-280q-26 0-48.5-11T392-320H200Zm280-40q17 0 28.5-11.5T520-400q0-17-11.5-28.5T480-440q-17 0-28.5 11.5T440-400q0 17 11.5 28.5T480-360Zm-280-40h160q0-50 35-85t85-35q50 0 85 35t35 85h160v-360H200v360Zm0 200h560-560Z"/></svg>
+                <p>Personalized Test</p> 
+                </Link>
+                <Link to='/messager-bot' className={activeLink === 8 ? "sidebar_link active" : "sidebar_link"} onClick={() => DashboardActivateLink(8)}>                    
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M200-120q-33 0-56.5-23.5T120-200v-400q0-100 70-170t170-70h240q100 0 170 70t70 170v400q0 33-23.5 56.5T760-120H200Zm0-80h560v-400q0-66-47-113t-113-47H360q-66 0-113 47t-47 113v400Zm160-280q-33 0-56.5-23.5T280-560q0-33 23.5-56.5T360-640q33 0 56.5 23.5T440-560q0 33-23.5 56.5T360-480Zm240 0q-33 0-56.5-23.5T520-560q0-33 23.5-56.5T600-640q33 0 56.5 23.5T680-560q0 33-23.5 56.5T600-480ZM280-200v-80q0-33 23.5-56.5T360-360h240q33 0 56.5 23.5T680-280v80h-80v-80h-80v80h-80v-80h-80v80h-80Zm-80 0h560-560Z"/></svg>
+                <p>Messager Bot</p> 
+                </Link>
+            </div>
+            <div className="sidebar_category">
+                <div className="sidebar_subcategory">PROFILE</div>
+                <Link to='/profiles' className={activeLink === 5 ? "sidebar_link active" : "sidebar_link"} onClick={() => DashboardActivateLink(5)}>                    
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M40-160v-112q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v112H40Zm720 0v-120q0-44-24.5-84.5T666-434q51 6 96 20.5t84 35.5q36 20 55 44.5t19 53.5v120H760ZM360-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47Zm400-160q0 66-47 113t-113 47q-11 0-28-2.5t-28-5.5q27-32 41.5-71t14.5-81q0-42-14.5-81T544-792q14-5 28-6.5t28-1.5q66 0 113 47t47 113ZM120-240h480v-32q0-11-5.5-20T580-306q-54-27-109-40.5T360-360q-56 0-111 13.5T140-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T440-640q0-33-23.5-56.5T360-720q-33 0-56.5 23.5T280-640q0 33 23.5 56.5T360-560Zm0 320Zm0-400Z"/></svg>
+                <p>Profiles</p>
+                </Link>
+                <Link to='/logout' className={activeLink === 6 ? "sidebar_link active" : "sidebar_link"} onClick={() => DashboardActivateLink(6)}>                    
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z"/></svg>
+                <p>Logout</p>
+                </Link>
+            </div>
+            <div className="sidebar_category">
+                <div className="sidebar_subcategory">CHARTS</div>
+                <Link to='/charts' className={activeLink === 7 ? "sidebar_link active" : "sidebar_link"} onClick={() => DashboardActivateLink(7)}>                    
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M160-200h160v-320H160v320Zm240 0h160v-560H400v560Zm240 0h160v-240H640v240ZM80-120v-480h240v-240h320v320h240v400H80Z"/></svg>
+                <p>Charts</p>
+                </Link>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Sidebar;
